80bc1b6f95df628dc877f4350565ac00c8fddf50
Ivanildo Lopes da Silva Junior <ivanildo.lopes.jr@gmail.com>
2020-03-04 19:43:08 -0300

    adicionando aulas modulo 03

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: 'GykTLqODQuU',
+        title: 'Formulário animado com JS e CSS | Mayk Brito',
+        duration: '57 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando o CSS GRID na prática | Mayk Brito',
+        duration: '36 min',
+        price: 'Free'
+    },
+    {
+        id: 'GSqR2i-Pq6o',
+        title: 'O que é API? Rest e Restfull | Mayk Brito',
+        duration: '33 min',
+        price: 'Free'
+    },
+    {
+        id: 'rAzHvYnQ8DY',
+        title: 'Array high order functions | Mayk Brito',
+        duration: '54 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'GykTLqODQuU',
+        title: 'Formulário animado com JS e CSS | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    },
+    {
+        id: 'vqrjFnq3-uo',
+        title: 'Criando player de áudio | Mayk Brito',
+        duration: '1h : 45min',
+        price: 'Free',
+        featured: true
+    }
+]
